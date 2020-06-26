bb90ae7f4a24662c8a23efba95946a4083ddba7c
LimitedSky <cruel2flow@naver.com>
2020-06-26 21:14:18 +0900

    WebFramework2 1392034 JeonghwanLim - SpringBoot report#3

---
+++ src/main/java/kr/ac/hansung/cse/repo/ProductRepository.java
@@ -0,0 +1,16 @@
+ package kr.ac.hansung.cse.repo;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.data.repository.CrudRepository;
+
+import kr.ac.hansung.cse.model.Product;
+
+//Instead of service, repository
+public interface ProductRepository extends CrudRepository<Product, Long> {
+	Optional<Product> findById(long id);
+
+	List<Product> findByCategory(String category);
+
+}
---
+++ src/main/resources/data.sql
@@ -0,0 +1,3 @@
+insert into product(name, category, price, manufacturer, unit_in_stock, description) values("product1", "category1", 100, "samsung", 10, "hello")
+insert into product(name, category, price, manufacturer, unit_in_stock, description) values("product2", "category1", 200, "lg", 20, "hi")
+insert into product(name, category, price, manufacturer, unit_in_stock, description) values("product3", "category2", 300, "lenovo", 30, "bye")
